a21b8e74a94369f36060c377c850c903e6a88378
Бедарев Евгений Владимирович <Bedarev.E.Vla@sberbank.ru>
2019-05-31 14:29:01 +0300

    first commit

--- src/Deployer.groovy
+++ src/Deployer.groovy
@@ -5,7 +5,7 @@ class Deployer {
     int tries = 0
     Script script
 
-    Deployer(Script script, String tries) {
+    Deployer(Script script, def tries) {
         this.tries = tries;
         this.script = script;
     }
