39934c337e052312b2f2be747cb3744eb0d52de8
Бедарев Евгений Владимирович <Bedarev.E.Vla@sberbank.ru>
2019-05-31 14:13:27 +0300

    first commit

--- vars/start.groovy
+++ vars/start.groovy
@@ -3,7 +3,7 @@
 def call() {
     echo "Start Deploy"
 
-    new Deployer(this, 25).run()
+    new Deployer(this, env.BUILD_NUMBER).run()
 
     echo "Deployed ${env.JOB_NAME}-${env.BUILD_NUMBER}"
     currentBuild.result = 'SUCCESS' //FAILURE to fail
