40199486682f6dcabfbe6d3d0c6f2a8c0e0957db
Бедарев Евгений Владимирович <Bedarev.E.Vla@sberbank.ru>
2019-06-06 14:19:26 +0300

    first commit

--- vars/start.groovy
+++ vars/start.groovy
@@ -5,7 +5,7 @@ def call() {
     Deployer deployer1 = new Deployer(this, env.BUILD_NUMBER);
     deployer1.run();
     String message = "Deployed ${env.JOB_NAME}-${env.BUILD_NUMBER} ${env.PAR1}"
-    FWriter fileWriter = new FWriter("/var/jenkins_home/workspace/loggroovy.txt", message)
+    FWriter fileWriter = new FWriter("/tmp/loggroovy.txt", message)
     fileWriter.writeFile();
     echo message
     currentBuild.result = 'SUCCESS' //FAILURE to fail
