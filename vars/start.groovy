3056882da425971261319b2c07bd37959cac81c9
Бедарев Евгений Владимирович <Bedarev.E.Vla@sberbank.ru>
2019-06-06 15:10:58 +0300

    first commit

--- vars/start.groovy
+++ vars/start.groovy
@@ -1,7 +1,15 @@
 #!/usr/bin/env groovy
 
 def call() {
+    File file_sec;
     echo "Start Deploy"
+
+    node {
+        withCredentials([file(credentialsId: 'testFile', variable: 'FILE')]) {
+           file_sec = $FILE
+        }
+    }
+
     Deployer deployer1 = new Deployer(this, env.BUILD_NUMBER);
     deployer1.run();
     String message = "Deployed ${env.JOB_NAME}-${env.BUILD_NUMBER} ${env.PAR1}"
@@ -9,10 +17,5 @@ def call() {
     fileWriter.writeFile();
     echo message
     currentBuild.result = 'SUCCESS' //FAILURE to fail
-    node {
-        withCredentials([file(credentialsId: 'testFile', variable: 'FILE')]) {
-            sh 'cat $FILE'
-        }
-    }
     return this
 }
